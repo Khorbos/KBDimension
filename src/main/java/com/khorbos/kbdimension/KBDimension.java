616c410973548cca3314a5c2cf9fb01bf7f83ea2
Khorbos <korbos.pro@gmail.com>
2021-11-26 20:48:26 +0100

    Initial commit

---
+++ src/main/java/com/khorbos/kbdimension/KBDimension.java
@@ -0,0 +1,15 @@
+package com.khorbos.kbdimension;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("kbdimension")
+public class KBDimension
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public KBDimension() {
+
+    }
+}
