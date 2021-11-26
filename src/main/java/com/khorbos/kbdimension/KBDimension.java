ed0b43187983b3bd23a7496c7e3d2702a4d07e01
Khorbos <korbos.pro@gmail.com>
2021-11-26 22:45:47 +0100

    feat(FMLEvent): edit main class, add FLM event methods (Common, Client setup and Server Starting)

--- src/main/java/com/khorbos/kbdimension/KBDimension.java
+++ src/main/java/com/khorbos/kbdimension/KBDimension.java
@@ -1,7 +1,11 @@
 package com.khorbos.kbdimension;
 
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,4 +20,11 @@ public class KBDimension
     public KBDimension() {
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
     }
+
+    private void setup(final FMLCommonSetupEvent event){}
+
+    private void doClientStuff(final FMLClientSetupEvent event){}
+
+    @SubscribeEvent
+    public void onServerStarting(final FMLServerStartingEvent event){}
 }
