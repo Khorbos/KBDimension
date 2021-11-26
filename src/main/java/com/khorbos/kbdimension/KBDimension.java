6c85c709173e8507edb18af04af6b2f8e3ff3db0
Khorbos <korbos.pro@gmail.com>
2021-11-26 22:44:00 +0100

    feat(EventBus): edit main class, add a MODID field property and add modEventBus

--- src/main/java/com/khorbos/kbdimension/KBDimension.java
+++ src/main/java/com/khorbos/kbdimension/KBDimension.java
@@ -1,15 +1,19 @@
 package com.khorbos.kbdimension;
 
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod("kbdimension")
+@Mod(KBDimension.ID)
+@Mod.EventBusSubscriber(modid = KBDimension.ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class KBDimension
 {
+    public static final String ID = "kbdimension";
     private static final Logger LOGGER = LogManager.getLogger();
 
     public KBDimension() {
-
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
     }
 }
