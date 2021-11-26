0bd1d6bcc66621a7c8d176ef06330d30f12590b2
Khorbos <korbos.pro@gmail.com>
2021-11-26 23:34:51 +0100

    feat(registry): add ModEventBus logic and listener to get items in-game

--- src/main/java/com/khorbos/kbdimension/KBDimension.java
+++ src/main/java/com/khorbos/kbdimension/KBDimension.java
@@ -1,13 +1,19 @@
 package com.khorbos.kbdimension;
 
 import com.khorbos.kbdimension.core.init.Registries;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.IForgeRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -20,6 +26,8 @@ public class KBDimension
 
     public KBDimension() {
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::setup);
+        modEventBus.addListener(this::doClientStuff);
 
         Registries.initRegistries(modEventBus);
     }
@@ -30,4 +38,18 @@ public class KBDimension
 
     @SubscribeEvent
     public void onServerStarting(final FMLServerStartingEvent event){}
+
+    @SubscribeEvent
+    public static void onRegisterItems(final RegistryEvent.Register<Item> event){
+        final IForgeRegistry<Item> registry = event.getRegistry();
+        Registries.BLOCKS.getEntries().stream()
+                .map(RegistryObject::get).forEach(block -> {
+                    final Item.Properties properties = new Item.Properties().group(ItemGroup.MISC);
+                    final BlockItem blockItem = new BlockItem(block, properties);
+                    blockItem.setRegistryName(block.getRegistryName());
+                    registry.register(blockItem);
+
+                });
+        KBDimension.LOGGER.debug("KBDimensionMod Registry Initialized");
+    }
 }
